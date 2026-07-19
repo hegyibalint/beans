41c21066230c3f2c08e9af2d71b25eba53c5c93f
Bálint Hegyi <balint@blnt.dev>
2026-07-19 21:49:33 +0100

    Reorganize core

--- crates/core/src/model.rs
+++ crates/core/src/model.rs
@@ -1,1 +1,16 @@
+use std::range::Range;
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct Span {
+    pub start: usize,
+    pub end: usize,
+}
+
+impl From<Range<usize>> for Span {
+    fn from(range: Range<usize>) -> Self {
+        Self {
+            start: range.start,
+            end: range.end,
+        }
+    }
+}
